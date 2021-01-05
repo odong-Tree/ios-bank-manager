ecb37973533b08908edd825709a67450e407469f
odong-Tree <ss12357@naver.com>
2021-01-05 22:48:10 +0900

    feat: main.swift에 ConsoleController 구조체 추가

--- BankManagerConsoleApp/BankManagerConsoleApp/main.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/main.swift
@@ -5,3 +5,34 @@
 // 
 
 import Foundation
+
+struct ConsoleController {
+    func printMenu() {
+        print(" 1 : 은행개점 \n 2 : 종료\n 입력 : ", terminator: "")
+        guard let userInput = readLine() else {
+            print("잘못된 입력값입니다. 다시 시도해주세요.")
+            printMenu()
+            return
+        }
+        
+        checkUserInput(userInput)
+    }
+    
+    func checkUserInput(_ input: String) {
+        switch input {
+        case "1":
+            print("시작")
+            let bankManager = BankManager()
+            bankManager.openBank(bankClerkNumber: 1)
+            printMenu()
+        case "2":
+            break
+        default :
+            print("잘못된 입력값입니다. 다시 시도해주세요.")
+            printMenu()
+        }
+    }
+}
+
+let consoleController = ConsoleController()
+consoleController.printMenu()
